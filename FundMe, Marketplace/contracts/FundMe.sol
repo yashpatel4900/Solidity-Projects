b93dd327b9a48afc86aa1ccf75293da8c9c4ec07
Yash Patel <62371168+yashpatel4900@users.noreply.github.com>
2022-08-25 21:13:27 +0530

    FundMe

---
+++ FundMe, Marketplace/contracts/FundMe.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./PriceConverter.sol";
+
+contract FundMe {
+    using PriceConverter for uint256;
+
+    address immutable i_owner;
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
+    uint256 public constant MINIMUM_USD = 50 * 10**18;
+
+    constructor() {
+        i_owner = msg.sender;
+    }
+
+    function Fund() public payable {
+        require(
+            msg.value.getConversionRate() > MINIMUM_USD,
+            "You need to send more ETH"
+        );
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] += msg.value;
+    }
+
+    function withdraw() public payable {
+        require(i_owner == msg.sender);
+
+        funders = new address[](0);
+        for (uint256 index = 0; index < funders.length; index++) {
+            address funder = funders[index];
+            addressToAmountFunded[funder] = 0;
+        }
+
+        (bool success, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(success, "Call Failed");
+    }
+}
